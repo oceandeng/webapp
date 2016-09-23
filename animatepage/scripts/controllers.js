6b300c7b71695a31c3c4bed56d175dfcaa6df766
denghaiyang <denghaiyang@300.cn>
2016-09-23 16:24:14 +0800

    update

--- animatepage/scripts/controllers.js
+++ animatepage/scripts/controllers.js
@@ -236,6 +236,19 @@ bookStoreCtrls.controller('UI', ['$rootScope', '$scope', function($rootScope, $s
         console.log($scope.form.$valid);
     }
 
+
+    var map = new BMap.Map("map");
+    map.centerAndZoom(new BMap.Point(116.404, 39.915), 10);
+
+    var p1 = new BMap.Point(116.301934,39.977552);
+    var p2 = new BMap.Point(116.508328,39.919141);
+
+    var driving = new BMap.DrivingRoute(map, {renderOptions:{map: map, autoViewport: true}});
+    driving.search(p1, p2);
+
+
+
+
 }]);
 
 bookStoreCtrls.controller('EditorCtrl', ['$scope', "$timeout", function($scope, $timeout){
